fe875e414f65a33caa167b63838791781af65085
Matt Lukso <mk.lukso@gmail.com>
2018-09-11 21:59:25 -0500

    messing with icons in about section

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -22,14 +22,15 @@ class About extends Component {
                   width: 200, 
                   height: 200, 
                   borderRadius: 200, 
-                  filter: 'grayscale(100%)',
+                  filter: 'grayscale(75%)',
                   padding: 20
                   }}/>
                 <p>  
                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                </p>        
-              </div>                       
-          </div>     
+                </p>   
+
+              </div>                     
+          </div>       
           <ScrollToNext pageSelector=".project-page"/>  
         </div>   
       </div>
